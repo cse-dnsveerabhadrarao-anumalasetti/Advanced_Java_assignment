51d53524b08bb25e1479bc8fa1d1f0a3a110da78
cse-dnsveerabhadrarao-anumalasetti <cse.veerabhadrarao.anumalasetti@gmail.com>
2026-02-12 21:12:00 +0530

    Update AccountClass.java

--- com.Capgemini.com.Capgemini.BankAccountBalanceValidationSystem/src/test/java/BankAccountBalanceValidationSystem/AccountClass.java
+++ com.Capgemini.com.Capgemini.BankAccountBalanceValidationSystem/src/test/java/BankAccountBalanceValidationSystem/AccountClass.java
@@ -2,4 +2,30 @@ package BankAccountBalanceValidationSystem;
 
 public class AccountClass {
 
+    private String accountNumber;
+    private String accountHolderName;
+    private double currentBalance;
+
+    public AccountClass(String accountNumber, String accountHolderName, double currentBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.currentBalance = currentBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(double currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
 }
