62ecb281733f504f2ece13dfbd2aa6c918a4c649
cse-dnsveerabhadrarao-anumalasetti <cse.veerabhadrarao.anumalasetti@gmail.com>
2026-02-12 21:14:01 +0530

    Update AccountService.java

--- com.Capgemini.com.Capgemini.BankAccountBalanceValidationSystem/src/test/java/BankAccountBalanceValidationSystem/AccountService.java
+++ com.Capgemini.com.Capgemini.BankAccountBalanceValidationSystem/src/test/java/BankAccountBalanceValidationSystem/AccountService.java
@@ -2,4 +2,30 @@ package BankAccountBalanceValidationSystem;
 
 public class AccountService {
 
+
+    private static final double MIN_BALANCE = 1000;
+
+    public double deposit(AccountClass account, double amount) {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Deposit must be greater than 0");
+        }
+
+        account.setCurrentBalance(account.getCurrentBalance() + amount);
+        return account.getCurrentBalance();
+    }
+
+    public boolean withdraw(AccountClass account, double amount) {
+
+        if (amount <= account.getCurrentBalance()) {
+            account.setCurrentBalance(account.getCurrentBalance() - amount);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkMinimumBalance(AccountClass account) {
+        return account.getCurrentBalance() >= MIN_BALANCE;
+    }
 }
